46f9ec334d4760698afa3a0041df790347ecb85a
jacques42 <github.com@weismantel.de>
2020-10-01 23:25:10 +0200

    added eslint checks

--- cli.js
+++ cli.js
@@ -2,202 +2,191 @@
 ** Command Line Options
 */
 
-opsParameter = {  jsonurl:        "http://127.0.0.1:8085/data.json",
-                        mqttBroker:     "mqtt://127.0.0.1:1883",
-                        mqttAuth:       false,
-                        mqttUsername:   "",
-                        mqttPassword:   "",
-                        mqttTopic:      "hosts",
-                        sendRawData:    false,
-                        sendFrequency:  2}
+const opsParameter = {
+  jsonurl: 'http://127.0.0.1:8085/data.json',
+  mqttBroker: 'mqtt://127.0.0.1:1883',
+  mqttAuth: false,
+  mqttUsername: '',
+  mqttPassword: '',
+  mqttTopic: 'hosts',
+  sendRawData: false,
+  sendFrequency: 2
+}
 
 const commandLineArgs = require('command-line-args')
 const commandLineUsage = require('command-line-usage')
 const validator = require('validator')
 
-const cliDefinition = [ 
-    { name: 'help', alias: 'h', type: Boolean, description: 'Display this usage guide.'  },
-    { name: 'jsonurl', type: String, description: "HTTP only URL to read the JSON data file from. Defaults to " + opsParameter.jsonurl },
-    { name: 'mqttBroker', type: String, description: "MQTT broker URL. Defaults to " + opsParameter.mqttBroker },
-    { name: 'mqttUsername', type: String, description: "Specify MQTT username to enable authentication"},
-    { name: 'mqttPassword', type: String, description: "Specify MQTT Password for authentication. Defaults to an empty string"},
-    { name: 'mqttTopic', type: String, description: "Specify MQTT topic to publish on. Hostname will be appended. Defaults to <"+ opsParameter.mqttTopic + ">"},
-    { name: 'raw', alias: 'r', type: Boolean, description: "Send raw JSON data to MQTT. Any filter settings will be ignored."},
-    { name: 'frequency', alias: 'f', type: Number, description: "Frequency to publish to MQTT in [s]. Default is " + opsParameter.sendFrequency},
-    { name: 'filterID', type: String, description: "List of comma-separated IDs to filter. First list item value defines whether list elements are allowed (value = 1) or denied (value = 0). Example: \"0,80,82\" denies measurement IDs 80 and 82" },
-    { name: 'filterList', type: String, description: "List of comma-separated strings to filter. First list item value defines whether list elements are allowed (value = allow) or denied (value = deny). Example: \"allow,Temperature, Mainboard\" allows measurements to be published where any node text in a given branch matches one of those strings" },
-    { name: 'cleanValues', alias: 'c', type: Boolean, description: 'Clean measurement values from all non-numeric characters'  },
+const cliDefinition = [
+  { name: 'help', alias: 'h', type: Boolean, description: 'Display this usage guide.' },
+  { name: 'jsonurl', type: String, description: 'HTTP only URL to read the JSON data file from. Defaults to ' + opsParameter.jsonurl },
+  { name: 'mqttBroker', type: String, description: 'MQTT broker URL. Defaults to ' + opsParameter.mqttBroker },
+  { name: 'mqttUsername', type: String, description: 'Specify MQTT username to enable authentication' },
+  { name: 'mqttPassword', type: String, description: 'Specify MQTT Password for authentication. Defaults to an empty string' },
+  { name: 'mqttTopic', type: String, description: 'Specify MQTT topic to publish on. Hostname will be appended. Defaults to <' + opsParameter.mqttTopic + '>' },
+  { name: 'raw', alias: 'r', type: Boolean, description: 'Send raw JSON data to MQTT. Any filter settings will be ignored.' },
+  { name: 'frequency', alias: 'f', type: Number, description: 'Frequency to publish to MQTT in [s]. Default is ' + opsParameter.sendFrequency },
+  { name: 'filterID', type: String, description: 'List of comma-separated IDs to filter. First list item value defines whether list elements are allowed (value = 1) or denied (value = 0). Example: "0,80,82" denies measurement IDs 80 and 82' },
+  { name: 'filterList', type: String, description: 'List of comma-separated strings to filter. First list item value defines whether list elements are allowed (value = allow) or denied (value = deny). Example: "allow,Temperature, Mainboard" allows measurements to be published where any node text in a given branch matches one of those strings' },
+  { name: 'cleanValues', alias: 'c', type: Boolean, description: 'Clean measurement values from all non-numeric characters' }
 ]
 
 const cliParameter = commandLineArgs(cliDefinition)
 
 if (cliParameter.help) {
-    const usage = commandLineUsage([
-        {
-          header: 'OWH2MQTT',
-          content: 'A simple tool to read OpenHardwareMonitor JSON data from it\'s webserver and publish to a MQTT server'
-        },
-        {
-          header: 'Options',
-          optionList: cliDefinition
-        },
-        {
-          content: 'Project home: {underline https://github.com/jacques42/ohw-mqtt}'
-        }
-      ])
-      console.log(usage)
-
-      process.exit();
-}
-
-if(cliParameter.jsonurl)
-{
-    if(! validator.isURL(cliParameter.jsonurl, { protocols: ['http'], require_protocol: true, require_tld: false})) {
-        throw new Error ("Not a valid URL: <" + cliParameter.jsonurl + ">")
-    } else {
-        opsParameter.jsonurl = cliParameter.jsonurl
-    }
-}
-
-if(cliParameter.mqttBroker)
-{
-    if(! validator.isURL(cliParameter.mqttBroker, { protocols: ['mqtt' , 'tcp', 'tls', 'mqtts'], require_protocol: true, require_tld: false})) {
-        throw new Error ("Not a valid MQTT URL: <" + cliParameter.mqttBroker + ">")
-    } else {
-        opsParameter.mqttBroker = cliParameter.mqttBroker
-    }
-}
-
-if(typeof cliParameter.mqttUsername !== 'undefined')
-{
-    if (cliParameter.mqttUsername === null) {
-        throw new Error ("No MQTT username provided")
-    }
-
-    if (typeof cliParameter.mqttPassword === 'undefined' || cliParameter.mqttPassword === null) {
-        throw new Error ("No MQTT password provided")
-    }
-
-    validator.trim(cliParameter.mqttUsername)
-    validator.trim(cliParameter.mqttPassword)
-
-    if (cliParameter.mqttUsername.length > 128 || ! validator.isAlphanumeric(cliParameter.mqttUsername))
+  const usage = commandLineUsage([
     {
-        throw new Error ("Not a valid mqttUsername - max. 128 alpha-numeric characters")
-    }
-    else {
-        opsParameter.mqttUsername = cliParameter.mqttUsername
-        opsParameter.mqttAuth = true
-    }
-
-    if (cliParameter.mqttPassword.length > 128 || ! validator.isAlphanumeric(cliParameter.mqttPassword) )
+      header: 'OWH2MQTT',
+      content: 'A simple tool to read OpenHardwareMonitor JSON data from it\'s webserver and publish to a MQTT server'
+    },
     {
-        throw new Error ("Not a valid mqttPassword - max. 128 alpha-numeric characters")
-    } else {
-        opsParameter.mqttPassword = cliParameter.mqttPassword
+      header: 'Options',
+      optionList: cliDefinition
+    },
+    {
+      content: 'Project home: {underline https://github.com/jacques42/ohw-mqtt}'
     }
+  ])
+  console.log(usage)
+
+  process.exit()
 }
 
-if(typeof cliParameter.mqttTopic !== 'undefined')
-{
-    if (cliParameter.mqttTopic === null || cliParameter.mqttTopic.length > 128 )
-    {
-        throw new Error ("Not a valid mqttTopic - max. 128 characters")
-    } else {
-        validator.trim(cliParameter.mqttTopic)
-        opsParameter.mqttTopic = cliParameter.mqttTopic.toLowerCase()
-    }
+if (cliParameter.jsonurl) {
+  if (!validator.isURL(cliParameter.jsonurl, { protocols: ['http'], require_protocol: true, require_tld: false })) {
+    throw new Error('Not a valid URL: <' + cliParameter.jsonurl + '>')
+  } else {
+    opsParameter.jsonurl = cliParameter.jsonurl
+  }
+}
+
+if (cliParameter.mqttBroker) {
+  if (!validator.isURL(cliParameter.mqttBroker, { protocols: ['mqtt', 'tcp', 'tls', 'mqtts'], require_protocol: true, require_tld: false })) {
+    throw new Error('Not a valid MQTT URL: <' + cliParameter.mqttBroker + '>')
+  } else {
+    opsParameter.mqttBroker = cliParameter.mqttBroker
+  }
+}
+
+if (typeof cliParameter.mqttUsername !== 'undefined') {
+  if (cliParameter.mqttUsername === null) {
+    throw new Error('No MQTT username provided')
+  }
+
+  if (typeof cliParameter.mqttPassword === 'undefined' || cliParameter.mqttPassword === null) {
+    throw new Error('No MQTT password provided')
+  }
+
+  validator.trim(cliParameter.mqttUsername)
+  validator.trim(cliParameter.mqttPassword)
+
+  if (cliParameter.mqttUsername.length > 128 || !validator.isAlphanumeric(cliParameter.mqttUsername)) {
+    throw new Error('Not a valid mqttUsername - max. 128 alpha-numeric characters')
+  } else {
+    opsParameter.mqttUsername = cliParameter.mqttUsername
+    opsParameter.mqttAuth = true
+  }
+
+  if (cliParameter.mqttPassword.length > 128 || !validator.isAlphanumeric(cliParameter.mqttPassword)) {
+    throw new Error('Not a valid mqttPassword - max. 128 alpha-numeric characters')
+  } else {
+    opsParameter.mqttPassword = cliParameter.mqttPassword
+  }
+}
+
+if (typeof cliParameter.mqttTopic !== 'undefined') {
+  if (cliParameter.mqttTopic === null || cliParameter.mqttTopic.length > 128) {
+    throw new Error('Not a valid mqttTopic - max. 128 characters')
+  } else {
+    validator.trim(cliParameter.mqttTopic)
+    opsParameter.mqttTopic = cliParameter.mqttTopic.toLowerCase()
+  }
 }
 
 if (cliParameter.raw) {
-    opsParameter.sendRawData = true;
+  opsParameter.sendRawData = true
 }
 
 if (cliParameter.frequency) {
-    if (! Number.isNaN(cliParameter.frequency) && 
+  if (!Number.isNaN(cliParameter.frequency) &&
         cliParameter.frequency >= 1 && cliParameter.frequency <= 600) {
-        opsParameter.sendFrequency = cliParameter.frequency;
-    } else {
-        throw new Error ("Frequency out of limits, must be 1-600 seconds")
-    }
+    opsParameter.sendFrequency = cliParameter.frequency
+  } else {
+    throw new Error('Frequency out of limits, must be 1-600 seconds')
+  }
 }
 
 if (cliParameter.frequency) {
-    if (! Number.isNaN(cliParameter.frequency) && 
+  if (!Number.isNaN(cliParameter.frequency) &&
         cliParameter.frequency >= 1 && cliParameter.frequency <= 600) {
-        opsParameter.sendFrequency = cliParameter.frequency;
-    } else {
-        throw new Error ("Frequency out of limits, must be 1-600 seconds")
-    }
+    opsParameter.sendFrequency = cliParameter.frequency
+  } else {
+    throw new Error('Frequency out of limits, must be 1-600 seconds')
+  }
 }
 
-if (typeof cliParameter.filterID !== 'undefined' && typeof cliParameter.filterList !== 'undefined')
-{
-    throw new Error ("Parameter filterID and filterList can not be used together")
+if (typeof cliParameter.filterID !== 'undefined' && typeof cliParameter.filterList !== 'undefined') {
+  throw new Error('Parameter filterID and filterList can not be used together')
 }
 
-if (typeof cliParameter.filterID !== 'undefined' && cliParameter.filterID ) {
+if (typeof cliParameter.filterID !== 'undefined' && cliParameter.filterID) {
+  var filters = cliParameter.filterID.split(',').map(Number)
 
-    var filters = cliParameter.filterID.split(',').map(Number)
+  switch (filters[0]) {
+    case 0:
+      opsParameter.filterMode = false
+      break
+    case 1:
+      opsParameter.filterMode = true
+      break
+    default:
+      throw new Error('ID filter first parameter must be 0 (deny) or 1 (allow)')
+  }
 
-    switch (filters[0]) {
-        case 0:
-            opsParameter.filterMode = false
-            break;
-        case 1: 
-            opsParameter.filterMode = true
-            break;
-        default:
-            throw new Error ("ID filter first parameter must be 0 (deny) or 1 (allow)")
-    }
+  filters = filters.filter(function (value) {
+    return !Number.isNaN(value)
+  })
 
-    filters = filters.filter(function (value) {
-        return !Number.isNaN(value);
-    });
+  filters.shift()
 
-    filters.shift()
+  if (!filters.length) {
+    throw new Error('ID filter need at least one valid filter parameter')
+  }
 
-    if (! filters.length)
-    {
-        throw new Error ("ID filter need at least one valid filter parameter")
-    }
-
-    opsParameter.filterPattern = filters
-    opsParameter.filterType = 'id'
+  opsParameter.filterPattern = filters
+  opsParameter.filterType = 'id'
 }
 
-if (typeof cliParameter.filterList !== 'undefined' && cliParameter.filterList ) {
+if (typeof cliParameter.filterList !== 'undefined' && cliParameter.filterList) {
+  filters = cliParameter.filterList.split(',')
 
-    var filters = cliParameter.filterList.split(',')
+  switch (filters[0]) {
+    case 'deny':
+      opsParameter.filterMode = false
+      break
+    case 'allow':
+      opsParameter.filterMode = true
+      break
+    default:
+      throw new Error('List filter first parameter must be "deny" or "allow"')
+  }
 
-    switch (filters[0]) {
-        case 'deny':
-            opsParameter.filterMode = false
-            break;
-        case 'allow': 
-            opsParameter.filterMode = true
-            break;
-        default:
-            throw new Error ("List filter first parameter must be \"deny\" or \"allow\"")
-    }
+  filters.shift()
 
-    filters.shift()
+  filters = filters.filter(function (value) {
+    return validator.isAlphanumeric(value)
+  })
 
-    filters = filters.filter(function (value) {
-        return validator.isAlphanumeric(value);
-    });
+  if (!filters.length) {
+    throw new Error('List filter need at least one valid filter parameter')
+  }
 
-    if (! filters.length)
-    {
-        throw new Error ("List filter need at least one valid filter parameter")
-    }
-
-    opsParameter.filterPattern = filters
-    opsParameter.filterType = 'text'
+  opsParameter.filterPattern = filters
+  opsParameter.filterType = 'text'
 }
 
 if (typeof cliParameter.cleanValues !== 'undefined') {
-    opsParameter.cleanValues = cliParameter.cleanValues
+  opsParameter.cleanValues = cliParameter.cleanValues
 }
 
 module.exports.jsonurl = opsParameter.jsonurl
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/* 
+/*
 ** Defaults + Commandline
-*/ 
+*/
 
 const config = require('./cli.js')
 
@@ -8,51 +8,53 @@ const config = require('./cli.js')
  ** Install Error Handler
  */
 
-process.on('uncaughtException', handleError);
+process.on('uncaughtException', handleError)
 
-function handleError(error) {
-    if (error.response) {
-        // The request was made and the server responded with a status code out of range of 2xx
-        handleError(new Error("URL GET response: <" +  error.response.status + ">"))
-    } else if (error.request) {
-        switch (error.errno) {
-            case 'ENOTFOUND':
-                handleError(new Error ("Unable to resolve / reach hostname <" + error.hostname + ">"))
-            case 'ECONNREFUSED':
-                handleError(new Error ("Connection refused by server <" + error.address + ">"))
-            default:
-                handleError(new Error("No URL GET response received <" + error.toJSON() + ">"))
-        }
-    } else {
-        console.error(error.toString())
-      }
-    
-    process.exit(1) //mandatory (as per the Node.js docs)
+function handleError (error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code out of range of 2xx
+    handleError(new Error('URL GET response: <' + error.response.status + '>'))
+  } else if (error.request) {
+    switch (error.errno) {
+      case 'ENOTFOUND':
+        handleError(new Error('Unable to resolve / reach hostname <' + error.hostname + '>'))
+        break
+      case 'ECONNREFUSED':
+        handleError(new Error('Connection refused by server <' + error.address + '>'))
+        break
+      default:
+        handleError(new Error('No URL GET response received <' + error.toJSON() + '>'))
+    }
+  } else {
+    console.error(error.toString())
   }
 
+  process.exit(1) // mandatory (as per the Node.js docs)
+}
 
 /*
 ** Establish MQTT Server Connection
 */
-const os=require('os')
-const mqttLib = require ('mqtt')
+const os = require('os')
+const mqttLib = require('mqtt')
 
-config.mqttTopic = config.mqttTopic.toLowerCase() +  "/" + os.hostname().toLowerCase()
+config.mqttTopic = config.mqttTopic.toLowerCase() + '/' + os.hostname().toLowerCase()
 
 const mqttClient = mqttLib.connect(config.mqttBroker, { username: config.mqttUsername, password: config.mqttPassword })
 
 mqttClient.on('error', function (err) {
-    // increase reconnect timer and exit if expired
-    handleError (new Error("MQTT " + err.toString()))
-  })
+  // increase reconnect timer and exit if expired
+  handleError(new Error('MQTT ' + err.toString()))
+})
 
 mqttClient.on('close', function (err) {
-    // reset reconnect timer
+  // do nothing
+  return err
 })
 
 mqttClient.on('connect', function () {
-    // do nothing
-  })
+  // do nothing
+})
 
 /*
 ** Open URL, Read file, send to MQTT
@@ -61,135 +63,113 @@ mqttClient.on('connect', function () {
 const httpClient = require('axios')
 
 function loopForever () {
-
-    httpClient.get(config.jsonurl)
-    .then(verifyServerResponse)    
+  httpClient.get(config.jsonurl)
+    .then(verifyServerResponse)
     .then(transformJSONData)
     .then(filterData)
-    .then(publishToMQTT) 
+    .then(publishToMQTT)
     .catch(handleError)
 
-    setTimeout(loopForever, config.sendFrequency * 1000)
+  setTimeout(loopForever, config.sendFrequency * 1000)
 }
 loopForever()
 
+function transformJSONData (data) {
+  // no data transformation when raw requested
+  if (config.sendRawData) { return data }
 
-function transformJSONData (data)
-{
-    // no data transformation when raw requested
-    if (config.sendRawData) { return data }
-
-    // Transform JSON data - mqttwarn can not process nested structures
-    return recursiveParser(data.Children)
-
+  // Transform JSON data - mqttwarn can not process nested structures
+  return recursiveParser(data.Children)
 }
 
-function recursiveParser (string1 , string2 = [], var1 = 0, array1 = [])
-{
-    let object = string1
-    // WHY ???
-    let branch = JSON.parse(JSON.stringify(string2))
-    let level = var1
-    let dataArray = array1
+function recursiveParser (string1, string2 = [], var1 = 0, array1 = []) {
+  const object = string1
+  // WHY ???
+  const branch = JSON.parse(JSON.stringify(string2))
+  const level = var1
+  let dataArray = array1
+  const re = config.cleanValues ? new RegExp('[^0-9,]', 'g') : ''
 
-    // Recursion clause
-    for (var property in object) {
-        // recursion if property is object (=> branch in JSON data set)
-        if (typeof object[property] === 'object')
-        {
-            if (typeof object.Text !== 'undefined')
-            {          
-                branch.push(object.Text)
-                dataArray = recursiveParser(object[property], branch, level + 1, dataArray )
-                branch.pop()
-            } else {
-                dataArray = recursiveParser(object[property], branch, level + 1, dataArray )
-            }
-        }
+  // Recursion clause
+  for (var property in object) {
+    // recursion if property is object (=> branch in JSON data set)
+    if (typeof object[property] === 'object') {
+      if (typeof object.Text !== 'undefined') {
+        branch.push(object.Text)
+        dataArray = recursiveParser(object[property], branch, level + 1, dataArray)
+        branch.pop()
+      } else {
+        dataArray = recursiveParser(object[property], branch, level + 1, dataArray)
+      }
     }
+  }
 
-    // Exit clause - value field set means measurement found ( => tree leaf node)
-    if (typeof object['Value'] !== 'undefined' && object['Value'].length > 0) {
+  // Exit clause - value field set means measurement found ( => tree leaf node)
+  if (typeof object.Value !== 'undefined' && object.Value.length > 0) {
+    // if (config.cleanValues) { re = new RegExp('[^0-9,]', 'g') } else { re = '' }
 
-        if (config.cleanValues)
-        { re = new RegExp("[^0-9,]","g") }
-        else 
-        { re = "" }
+    const jsonString = '{"Value":"' + object.Value.replace(re, '') + '",' +
+                            '"Max":"' + object.Max.replace(re, '') + '",' +
+                            '"Min":"' + object.Min.replace(re, '') + '"}'
 
-        let jsonString =  "{\"Value\":\"" + object['Value'].replace(re ,"") + "\"," +
-                            "\"Max\":\"" + object['Max'].replace(re ,"") + "\"," +
-                            "\"Min\":\"" + object['Min'].replace(re ,"") + "\"}";
+    branch.push(object.Text)
+    branch.push(jsonString)
+    dataArray.push(branch)
+  }
 
-        branch.push(object['Text'])
-        branch.push(jsonString)
-        dataArray.push(branch)    
-    }
-
-    // catch all return
-    return dataArray
+  // catch all return
+  return dataArray
 }
-
 
 function filterData (data) {
-    
-    // no filter applied for raw data to be sent
-    if (typeof config.filterMode === 'undefined')
-    {
-        return data
+  // no filter applied for raw data to be sent
+  if (typeof config.filterMode === 'undefined') {
+    return data
+  }
+
+  // filter by ID or Text value - filterMode defines ALLOW or DENY approach when match is found
+  return data.filter(element => {
+    switch (config.filterType) {
+      case 'id':
+        if (config.filterPattern.indexOf(element[0]) !== -1) {
+          return config.filterMode
+        }
+        break
+      case 'text':
+        if (config.filterPattern.filter(pattern => {
+          return (element.indexOf(pattern) !== -1)
+        }).length > 0) {
+          return config.filterMode
+        }
+        break
+      default:
+        break
     }
 
-    // filter by ID or Text value - filterMode defines ALLOW or DENY approach when match is found
-    return data.filter(element => {
-        switch (config.filterType) {
-            case 'id':
-                if (config.filterPattern.indexOf(element[0]) !== -1)
-                {
-                    return config.filterMode
-                }
-                break;
-            case 'text':
-                if (config.filterPattern.filter(pattern => { 
-                    return  (element.indexOf(pattern) !== -1 )
-                    }).length > 0)
-                    {
-                        return config.filterMode
-                    }
-                break;
-            default:
-                break
-            }
-
-        return ! config.filterMode
-        });
+    return !config.filterMode
+  })
 }
 
+function publishToMQTT (data) {
+  // console.log ("###########################################################################################")
 
-function publishToMQTT (data)
-{
-    console.log ("###########################################################################################")
+  // no data transformation when raw requested
+  if (config.sendRawData) {
+    mqttClient.publish(config.mqttTopic, JSON.stringify(data))
+  } else {
+    // Publish by topic
+    data.forEach(element => {
+      var mqttData = element.pop()
+      var mqttTopic = config.mqttTopic + '/' + element.join('/').replace(/[#+]/g, '').replace(/\s/g, '-')
+      //        console.log(mqttTopic + " -> " + mqttData)
 
-    // no data transformation when raw requested
-    if (config.sendRawData) {
-        mqttClient.publish(config.mqttTopic, JSON.stringify(data))
-    } else
-    {
-        // Publish by topic
-        data.forEach(element => { 
-            var mqttData = element.pop()
-            var mqttTopic = config.mqttTopic + "/" + element.join('/').replace(/[#+]/g, "").replace(/\s/g, "-")
-            console.log(mqttTopic + " -> " + mqttData)
-
-            mqttClient.publish(mqttTopic,mqttData)
-
-       });
-    }
+      mqttClient.publish(mqttTopic, mqttData)
+    })
+  }
 }
 
-function verifyServerResponse (obj)
-{
-    if (obj.headers["content-type"] !== "application/json")
-        { handleError (new Error("Data from Webserver URL is not in JSON format (content type <" + obj.headers["content-type"] + ">)")) }
-        
-    return obj.data
+function verifyServerResponse (obj) {
+  if (obj.headers['content-type'] !== 'application/json') { handleError(new Error('Data from Webserver URL is not in JSON format (content type <' + obj.headers['content-type'] + '>)')) }
+
+  return obj.data
 }
-  
